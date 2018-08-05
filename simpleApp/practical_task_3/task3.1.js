8636469e8503453ef35eefe46eb109c83575cd7f
annashvets <41542405+annashvets@users.noreply.github.com>
2018-08-05 14:33:46 +0300

    Delete task3.1.js

--- simpleApp/practical_task_3/task3.1.js
+++
@@ -1,9 +0,0 @@
-//Write a JavaScript function to get the last element of an array
-
-let mass = [5, 10, 25, 45, 100];
-
-function last(){
-    let lastelement = mass.pop();
-    return lastelement;
-}
- console.log(last());
