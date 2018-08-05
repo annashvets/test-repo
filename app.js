26b851faa8cefaf33c9238bcb6621082e9bdd77d
bob <bob@bobs-MacBook-Pro.local>
2018-08-05 15:16:26 +0300

    first commit app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("\nNice work! Your first application have been launched.");
+console.log("Now, I need to know your name, type it below: ");
+
+let stdin = process.openStdin();
+
+stdin.addListener("data", (txt) => {
+    if(txt.toString().trim() === "quit") {
+        console.log("\nHave a nice JS trip! Bye-bye");
+        stdin.end();
+    } else {
+        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+    }   
+  });
