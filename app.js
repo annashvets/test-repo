7a330d7d6e816208fb57adf217aa3019c1223168
bob <bob@bobs-MacBook-Pro.local>
2018-08-05 20:43:34 +0300

    calculator commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("\nNice work! Your first application have been launched.");
+/*console.log("\nNice work! Your first application have been launched.");
 console.log("Now, I need to know your name, type it below: ");
 
 let stdin = process.openStdin();
@@ -11,4 +11,31 @@ stdin.addListener("data", (txt) => {
         console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
         console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
     }   
-  });
+  });*/
+
+  function sum(a, b) {
+      let c = a + b;
+      return c;
+  };
+  function dif(a, b){
+      let c = a - b;
+      return c;
+  };
+  function mul(a, b){
+      let c = a * b;
+      return c;
+  };
+  function div(a, b){
+      let c;
+      if (b > 0){
+          c = a / b
+      } else {
+          c = "The number can't be '0'";
+      }
+      return c;
+  };
+
+  console.log(div(2, 0));
+
+
+
