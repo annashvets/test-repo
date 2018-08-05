5d9fdcb52ffcc2164abed54eac17af76034ca642
annashvets <41542405+annashvets@users.noreply.github.com>
2018-08-05 14:33:17 +0300

    Delete task2.3.js

--- simpleApp/practical_task_2/task2.3.js
+++
@@ -1,9 +0,0 @@
-abbrev = function (str){
-    let a = str.split(" ");
-    let res1 = a[0].charAt(0);
-    let res2 = a[1].charAt(0);
-    let res = res1 + "." + res2 + ".";
-    return res;
-}
-
-console.log(abbrev(`Anna Shvets`));
